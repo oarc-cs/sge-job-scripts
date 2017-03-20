65f29bc5e0055dcfa26061d77895ffc8b31dd789
Shao-Ching Huang <schuang@localhost>
2017-03-20 12:13:40 -0700

    first commit

---
+++ mpi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# SGE job script: distributed-memory/MPI job (using multiple compute nodes)
+#
+#$ -j y
+#$ -o stdout.$JOB_ID
+#$ -cwd
+#$ -l h_data=2G,h_rt=24:00:00
+#$ -pe dc* 8
+#
+# Please note:
+# - Number of slots (-pe) cannot exceeds the total number of CPU cores of the
+#   compute node being requested
+#
+# load modules into your environment
+source /u/local/Modules/default/init/modules.sh
+module load intel
+module load python
+
+# source your own startup script if needed
+# source ~/.bash_profile
+
+# run the executable
+mpirun -np $NSLOTS ./a.out
+
---
+++ seq-ja.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# SGE job script: sequential job array
+#
+#$ -j y
+#$ -o stdout.$JOB_ID.$SGE_TASK_ID
+#$ -cwd
+#$ -l h_data=2G,h_rt=24:00:00
+#$ -t 1-10
+
+# Please note:
+# - The h_data value cannot exceed the physical memory size of the
+#   compute node being requested. 
+# - Each job has the same job number but a different task ID
+#   (environment variable $SGE_TASK_ID defined at runtime).
+#   The range of the task IDs is set by the "-t" option above.
+
+source /u/local/Modules/default/init/modules.sh
+module load intel
+module load python
+
+# source your own startup script if needed
+# source ~/.bash_profile
+
+# run the executable
+./a.out $SGE_TASK_ID
+
