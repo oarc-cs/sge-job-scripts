c53439f3de878e451b57df9df2a2f61079255891
Shao-Ching Huang <4-schuang@users.noreply.gitlab.idre.ucla.edu>
2021-04-27 11:18:14 -0700

    Update nb6.sh

--- nb6.sh
+++ nb6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -l h_rt=8:00:00,h_vmem=infinity,h_data=8G,arch=intel*
+#$ -l h_rt=8:00:00,h_data=8G,arch=intel*
 #$ -cwd
 #$ -N jupyter-c6
 #$ -j y
@@ -10,7 +10,9 @@ unset XDG_RUNTIME_DIR
 source /u/local/Modules/default/init/modules.sh
 module load anaconda3/2020.02
 
-# uncomment the "token" and "password" options below if you want to run jupyter notebook INSECURELY
-jupyter-notebook --ip=$(hostname -i | awk '{print $1}') --port=12345 --no-browser --notebook-dir=$HOME \
-                 #--NotebookApp.token='' #--NotebookApp.password=''
+# To run notebook INSECURELY without password/token, add: --NotebookApp.token='' --NotebookApp.password=''
+# The actual port number may be different, since Jupyter automatically retries (see --port-retries)
+
+jupyter-notebook --ip=$(hostname -i | awk '{print $1}') --port=12345 --no-browser --notebook-dir=$HOME
+
 
